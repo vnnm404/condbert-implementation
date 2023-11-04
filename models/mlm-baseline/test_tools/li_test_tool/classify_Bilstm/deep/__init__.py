cb469901b468ff6899b0f36ec9ac85b427fb64a6
Haran Raajesh <haran@Haran-Raajesh.local>
2023-11-04 23:16:15 +0530

    Removed models

--- models/mlm-baseline/test_tools/li_test_tool/classify_Bilstm/deep/__init__.py
+++
@@ -1,2 +0,0 @@
-import os
-os.sys.path.append(os.path.split(os.path.realpath(__file__))[0])
--- models/mlm-baseline/test_tools/li_test_tool/classify_Bilstm/deep/util/config.py
+++
@@ -1,8 +0,0 @@
-﻿import theano
-
-
-def globalFloatType():
-    return theano.config.floatX
-
-def globalCharSet() :
-    return 'gb18030'
